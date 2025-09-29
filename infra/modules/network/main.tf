71be22a0b09c05311db1b2522892b88f1684c392
redsquirrelx <redsquirrel__@hotmail.com>
2025-09-28 19:06:16 -0500

    feat: definir security groups

--- infra/modules/network/main.tf
+++ infra/modules/network/main.tf
@@ -14,3 +14,45 @@ resource "aws_vpc" "this" {
         Name = "main-vpc"
     }
 }
+
+## ALB-SG
+resource "aws_security_group" "alb-sg" {
+    vpc_id = aws_vpc.this.id
+    name = "alb-sg"
+    tags = {
+        Name = "alb-sg"
+    }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "alb-sg-ingress" {
+    security_group_id = aws_security_group.alb-sg.id
+    cidr_ipv4 = "0.0.0.0/0"
+    ip_protocol = "-1"
+}
+
+resource "aws_vpc_security_group_egress_rule" "alb-sg-egress" {
+    security_group_id = aws_security_group.alb-sg.id
+    cidr_ipv4 = "0.0.0.0/0"
+    ip_protocol = "-1"
+}
+
+## SERVICES-SG
+resource "aws_security_group" "services-sg" {
+    vpc_id = aws_vpc.this.id
+    name = "services-sg"
+    tags = {
+        Name = "services-sg"
+    }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "services-sg-ingress" {
+   security_group_id = aws_security_group.services-sg.id
+   referenced_security_group_id = aws_security_group.alb-sg.id
+   ip_protocol = "-1"
+}
+
+resource "aws_vpc_security_group_egress_rule" "services-sg-egress" {
+    security_group_id = aws_security_group.services-sg.id
+    cidr_ipv4 = "0.0.0.0/0"
+    ip_protocol = "-1"
+}
