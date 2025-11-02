5513369fad7c274c7a4202961b9911d251ef0a0a
redsquirrelx <redsquirrel.xi@gmail.com>
2025-11-02 18:20:29 -0500

    feat(certs): agregar cert. para el back

--- infra/certs.tf
+++ infra/certs.tf
@@ -35,3 +35,53 @@ resource "aws_acm_certificate_validation" "us_east_1" {
     certificate_arn         = aws_acm_certificate.us_east_1.arn
     validation_record_fqdns = [for record in aws_route53_record.us_east_1_cert : record.fqdn]
 }
+
+# Certificados para apigateway
+locals {
+    certificados = {
+        us-east-2 = {}
+        eu-west-1 = {}
+    }
+}
+
+resource "aws_acm_certificate" "api" {
+    for_each = local.certificados
+    region = each.key
+
+    domain_name       = var.domain_name
+    validation_method = "DNS"
+
+    subject_alternative_names = [
+        "api.${ var.domain_name }"
+    ]
+
+    lifecycle {
+        create_before_destroy = true
+    }
+
+    depends_on = [ aws_acm_certificate_validation.us_east_1 ]
+}
+
+resource "aws_route53_record" "api_cert" {
+    for_each = {
+        for dvo in aws_acm_certificate.api["us-east-2"].domain_validation_options : dvo.domain_name => {
+            name   = dvo.resource_record_name
+            record = dvo.resource_record_value
+            type   = dvo.resource_record_type
+        }
+    }
+
+    allow_overwrite = true
+    name            = each.value.name
+    records         = [each.value.record]
+    ttl             = 60
+    type            = each.value.type
+    zone_id         = aws_route53_zone.this.zone_id
+}
+
+resource "aws_acm_certificate_validation" "api_cert_val" {
+    region = "us-east-2"
+
+    certificate_arn         = aws_acm_certificate.api["us-east-2"].arn
+    validation_record_fqdns = [for record in aws_route53_record.api_cert : record.fqdn]
+}
