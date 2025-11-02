b762e908db55014866a9202b606f1a5599fdc81f
redsquirrelx <redsquirrel.xi@gmail.com>
2025-11-02 18:26:00 -0500

    chore(endpoints): instanciarlos un solo 1 AZ

--- infra/modules/network/main.tf
+++ infra/modules/network/main.tf
@@ -134,7 +134,7 @@ resource "aws_vpc_endpoint" "ecr_api" {
     vpc_id            = aws_vpc.this.id
     service_name      = "com.amazonaws.${data.aws_region.current.region}.ecr.api"
     vpc_endpoint_type = "Interface"
-    subnet_ids        = [ for subnet in aws_subnet.service-subnets: subnet.id ]
+    subnet_ids        = [ aws_subnet.service-subnets[0].id ]
     security_group_ids = [ aws_security_group.endpoints.id ]
     private_dns_enabled = true
 }
@@ -143,7 +143,7 @@ resource "aws_vpc_endpoint" "ecr_dkr" {
     vpc_id            = aws_vpc.this.id
     service_name      = "com.amazonaws.${data.aws_region.current.region}.ecr.dkr"
     vpc_endpoint_type = "Interface"
-    subnet_ids        = [ for subnet in aws_subnet.service-subnets: subnet.id ]
+    subnet_ids        = [ aws_subnet.service-subnets[0].id ]
     security_group_ids = [ aws_security_group.endpoints.id ]
     private_dns_enabled = true
 }
