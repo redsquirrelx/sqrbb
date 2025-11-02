6bb06de80d7cca859a5cedfbfbc39bea4a8e1b5a
redsquirrelx <redsquirrel.xi@gmail.com>
2025-11-02 18:26:53 -0500

    feat(vpc): habilitar flow log

--- infra/modules/network/main.tf
+++ infra/modules/network/main.tf
@@ -18,6 +18,59 @@ resource "aws_vpc" "this" {
     }
 }
 
+resource "aws_cloudwatch_log_group" "flow_log" {
+    name = "flow_log"
+    retention_in_days = 365
+}
+
+data "aws_iam_policy_document" "vpc_assume_role" {
+    statement {
+        effect = "Allow"
+
+        principals {
+            type        = "Service"
+            identifiers = [ "vpc-flow-logs.amazonaws.com" ]
+        }
+
+        actions = ["sts:AssumeRole"]
+    }
+}
+
+resource "aws_iam_role" "vpc" {
+    name               = "vpc_role"
+    assume_role_policy = data.aws_iam_policy_document.vpc_assume_role.json
+}
+
+data "aws_iam_policy_document" "vpc" {
+    statement {
+        effect = "Allow"
+
+        actions = [
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents",
+            "logs:DescribeLogGroups",
+            "logs:DescribeLogStreams"
+        ]
+
+        resources = [
+            aws_cloudwatch_log_group.flow_log.arn
+        ]
+    }
+}
+
+resource "aws_iam_role_policy" "vpc" {
+    role   = aws_iam_role.vpc.id
+    policy = data.aws_iam_policy_document.vpc.json
+}
+
+resource "aws_flow_log" "vpc" {
+    iam_role_arn = aws_iam_role.vpc.arn
+    log_destination = aws_cloudwatch_log_group.flow_log.arn
+    traffic_type = "ALL"
+    vpc_id = aws_vpc.this.id
+}
+
 #### SECURITY GROUPS
 
 ## DEFAULT
