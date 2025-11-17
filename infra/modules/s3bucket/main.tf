df091889e4df6440c6897229e95afa18d1dc82c6
redsquirrelx <redsquirrel.xi@gmail.com>
2025-11-16 22:57:54 -0500

    feat(s3bucket): definir recursos extra dentro del módulo entre ellas: - access_logs - notifications - lifecycle_config

--- infra/modules/s3bucket/main.tf
+++ infra/modules/s3bucket/main.tf
@@ -7,9 +7,6 @@ terraform {
 }
 
 resource "aws_s3_bucket" "this" {
-# checkov:skip=CKV_AWS_144:No necesita replicación cross-region
-# checkov:skip=CKV2_AWS_62:No necesita event notifications
-# checkov:skip=CKV_AWS_18:No necesita access logging
     region = var.region
 
     bucket = var.bucket_name
@@ -49,4 +46,93 @@ resource "aws_s3_bucket_public_access_block" "this" {
     block_public_policy     = true
     ignore_public_acls      = true
     restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_logging" "this" {
+    region = var.region
+
+    count = var.enable_access_logs ? 1 : 0
+
+    bucket = aws_s3_bucket.this.bucket
+    target_bucket = var.bucket_access_logs_bucket
+    target_prefix = "/s3_bucket_logs/${var.bucket_name}"
+}
+
+data "aws_iam_policy_document" "assume_role" {
+    statement {
+        effect = "Allow"
+
+        principals {
+            type        = "Service"
+            identifiers = ["s3.amazonaws.com"]
+        }
+
+        actions = ["sts:AssumeRole"]
+    }
+}
+
+resource "aws_iam_role" "this" {
+    count = var.replicate ? 1 : 0
+
+    name               = "s3bucket-replication"
+    assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_s3_bucket_replication_configuration" "this" {
+    region = var.region
+
+    count = var.replicate ? 1 : 0
+
+    bucket = aws_s3_bucket.this.id
+    role = aws_iam_role.this[0].arn
+
+    rule {
+        status = "Enabled"
+        destination {
+            bucket = var.bucket_replicated_id
+            storage_class = "STANDARD"
+        }
+    }
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+    region = var.region
+
+    count = var.enable_event_notifs ? 1 : 0
+
+    bucket = aws_s3_bucket.this.id
+
+    topic {
+        topic_arn     = var.sns_arn
+        events        = ["s3:ObjectCreated:*"]
+        filter_prefix = "logs/"
+    }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "this" {
+    region = var.region
+    
+    bucket = aws_s3_bucket.this.id
+
+    rule {
+        id = "1"
+        status = "Enabled"
+
+        filter {}
+
+        expiration {
+          days = 365
+        }
+    }
+
+    rule {
+        id = "2"
+        status = "Enabled"
+
+        filter {}
+
+        abort_incomplete_multipart_upload {
+          days_after_initiation = 1
+        }
+    }
 }
--- infra/modules/s3bucket/variables.tf
+++ infra/modules/s3bucket/variables.tf
@@ -6,3 +6,29 @@ variable "bucket_name" {
   
 }
 
+variable "enable_access_logs" {
+    type = bool
+}
+
+variable "bucket_access_logs_bucket" {
+    nullable = true
+    default = null
+}
+
+variable "replicate" {
+    type = bool
+}
+
+variable "bucket_replicated_id" {
+    nullable = true
+    default = null
+}
+
+variable "enable_event_notifs" {
+    type = bool
+}
+
+variable "sns_arn" {
+    nullable = true
+    default = null
+}
