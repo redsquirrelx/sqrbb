295d6df3bf91a7193e1648b02cf57d397888ff63
redsquirrelx <redsquirrel.xi@gmail.com>
2025-11-16 23:44:21 -0500

    feat(ecr): configurar kms

--- infra/modules/ecr/main.tf
+++ infra/modules/ecr/main.tf
@@ -34,6 +34,10 @@ resource "aws_ecr_repository" "msrvc-reservas" {
     image_scanning_configuration {
         scan_on_push = true
     }
+
+    encryption_configuration {
+        encryption_type = "KMS"
+    }
 }
 
 resource "aws_ecr_repository" "msrvc-propiedades" {
@@ -42,6 +46,10 @@ resource "aws_ecr_repository" "msrvc-propiedades" {
     
     image_scanning_configuration {
         scan_on_push = true
+    }
+
+    encryption_configuration {
+        encryption_type = "KMS"
     }
 }
 
