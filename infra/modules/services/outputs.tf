bd0dc0db28cb67186ff880387accd0f725b064b8
redsquirrelx <redsquirrel.xi@gmail.com>
2025-11-16 23:35:25 -0500

    feat(services): exportar arn de las tasks_definitions

--- infra/modules/services/outputs.tf
+++ infra/modules/services/outputs.tf
@@ -1,3 +1,11 @@
 output "cluster-main" {
     value = aws_ecs_cluster.this
 }
+
+output "propiedades_td_arn" {
+    value = aws_ecs_task_definition.this["propiedades"].arn
+}
+
+output "reservas_td_arn" {
+    value = aws_ecs_task_definition.this["reservas"].arn
+}
