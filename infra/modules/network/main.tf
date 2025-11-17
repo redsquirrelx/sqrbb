6425e2f9bfea21866a17687a118a752fa67a3322
redsquirrelx <redsquirrel.xi@gmail.com>
2025-11-16 23:44:41 -0500

    fix(dynamodb): agregar endpoint en vpc

--- infra/modules/network/main.tf
+++ infra/modules/network/main.tf
@@ -210,3 +210,10 @@ resource "aws_vpc_endpoint" "s3" {
     vpc_endpoint_type = "Gateway"
     route_table_ids   = [ aws_route_table.service.id ]
 }
+
+resource "aws_vpc_endpoint" "dynamodb" {
+    vpc_id            = aws_vpc.this.id
+    service_name = "com.amazonaws.${data.aws_region.current.region}.dynamodb"
+    vpc_endpoint_type = "Gateway"
+    route_table_ids   = [ aws_route_table.service.id ]
+}
