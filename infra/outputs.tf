ccdceccf7b27f535be90f8d3d90b45baa7c347b9
redsquirrelx <redsquirrel.xi@gmail.com>
2025-11-19 00:36:00 -0500

    chore(infra): reemplazar - por _ en outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,20 +1,20 @@
-output "cloudfront-dist-id" {
+output "cloudfront_dist_id" {
     value = module.cloudfront.cloudfront-dist-id
 }
 
-output "aws-account-id" {
+output "aws_account_id" {
     value = data.aws_caller_identity.current.account_id
 }
 
-output "lmbd-fn-sigv4a-arn" {
+output "lmbd_fn_sigv4a_arn" {
     value = module.lambda.lambda-fn-sigv4a.function_name
 }
 
-output "sigv4a-signer-name" {
+output "sigv4a_signer_name" {
     value = module.lambda.sigv4a-signer-name
 }
 
-output "cluster-main-name" {
+output "cluster_main_name" {
     value = module.services.cluster-main.name
 }
 
