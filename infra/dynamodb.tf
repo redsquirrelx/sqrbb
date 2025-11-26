c9ea804b1604ddf68152a052f0a199465ee02cad
redsquirrelx <redsquirrel.xi@gmail.com>
2025-11-25 19:37:21 -0500

    feat(dynamodb): nueva tabla para reservas

--- infra/dynamodb.tf
+++ infra/dynamodb.tf
@@ -41,3 +41,46 @@ resource "aws_dynamodb_table" "propiedades" {
     stream_enabled = true
     stream_view_type = "NEW_AND_OLD_IMAGES"
 }
+
+# Creacion de la tabla DynamoDB
+resource "aws_dynamodb_table" "reservas" {
+    region = "us-east-2"
+
+    name = "Reservas"
+    billing_mode = "PAY_PER_REQUEST"  # Se paga solamente cuando se consume el servicio
+
+    #Configuracion de la llave primaria
+    hash_key = "ReservaID"  # Se puede cambiar por una mas segura
+
+    # Llave de ordenamiento
+    range_key = "FechaRegistro"
+
+    # Se define el atributo de la llave primaria
+    attribute {
+        name = "ReservaID"
+        type = "S"  # S: String
+    }
+
+    # Se define la llave de ordenamiento
+    attribute {
+        name = "FechaRegistro"
+        type = "N"
+    }
+
+    ttl {
+        attribute_name = "expire_at"
+        enabled = true
+    }
+
+    point_in_time_recovery {
+        enabled = true
+    }
+
+    server_side_encryption {
+        enabled = true
+        kms_key_arn = null
+    }
+
+    stream_enabled = true
+    stream_view_type = "NEW_AND_OLD_IMAGES"
+}
