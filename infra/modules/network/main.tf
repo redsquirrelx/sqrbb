eb4e198f44244f8688a993950d269b95e00bc7be
redsquirrelx <redsquirrel.xi@gmail.com>
2025-11-25 19:22:16 -0500

    feat(network): config. endpoint para sns

--- infra/modules/network/main.tf
+++ infra/modules/network/main.tf
@@ -204,6 +204,15 @@ resource "aws_vpc_endpoint" "ecr_dkr" {
     private_dns_enabled = true
 }
 
+resource "aws_vpc_endpoint" "sns" {
+    vpc_id            = aws_vpc.this.id
+    service_name      = "com.amazonaws.${data.aws_region.current.region}.sns"
+    vpc_endpoint_type = "Interface"
+    subnet_ids        = [ aws_subnet.service-subnets[0].id ]
+    security_group_ids = [ aws_security_group.endpoints.id ]
+    private_dns_enabled = true
+}
+
 resource "aws_vpc_endpoint" "s3" {
     vpc_id            = aws_vpc.this.id
     service_name      = "com.amazonaws.${data.aws_region.current.region}.s3"
