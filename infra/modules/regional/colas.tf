d963de667190519c660fd2fac8e65c4c54097022
redsquirrelx <redsquirrel.xi@gmail.com>
2025-11-27 21:14:42 -0500

    chore(colas): mover a modulo "regional"

---
+++ infra/modules/regional/colas.tf
@@ -0,0 +1,45 @@
+resource "aws_sns_topic" "reserva_proc" {
+    region = var.region
+
+    name = "reserva_proc_topic"
+    # AWS Managed
+    kms_master_key_id = "alias/aws/sns"
+}
+
+resource "aws_sqs_queue" "error" {
+    region = var.region
+
+    name                      = "error_sqs"
+    sqs_managed_sse_enabled = true
+}
+
+module "stats_sqs" {
+    source = "../sqs"
+    region = var.region
+
+    sns_topic_arn = aws_sns_topic.reserva_proc.arn
+    name = "stats_sqs"
+    dlq_arn = aws_sqs_queue.error.arn
+}
+
+module "reserva_mail_sqs" {
+    source = "../sqs"
+    region = var.region
+
+    sns_topic_arn = aws_sns_topic.reserva_proc.arn
+    name = "reserva_mail_sqs"
+    dlq_arn = aws_sqs_queue.error.arn
+}
+
+resource "aws_sqs_queue_redrive_allow_policy" "terraform_queue_redrive_allow_policy" {
+    region = var.region
+    queue_url = aws_sqs_queue.error.id
+
+    redrive_allow_policy = jsonencode({
+        redrivePermission = "byQueue",
+        sourceQueueArns   = [
+            module.stats_sqs.sqs_arn,
+            module.reserva_mail_sqs.sqs_arn
+        ]
+    })
+}
