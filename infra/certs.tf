1587364bb0f78c267792614f2b71f9d1c94da426
redsquirrelx <redsquirrel.xi@gmail.com>
2025-11-27 21:19:43 -0500

    feat(certs): certificados para eu-west-1

--- infra/certs.tf
+++ infra/certs.tf
@@ -79,9 +79,33 @@ resource "aws_route53_record" "api_cert" {
     zone_id         = aws_route53_zone.this.zone_id
 }
 
+resource "aws_route53_record" "api_cert_eu_west_1" {
+    for_each = {
+        for dvo in aws_acm_certificate.api["eu-west-1"].domain_validation_options : dvo.domain_name => {
+            name   = dvo.resource_record_name
+            record = dvo.resource_record_value
+            type   = dvo.resource_record_type
+        }
+    }
+
+    allow_overwrite = true
+    name            = each.value.name
+    records         = [each.value.record]
+    ttl             = 60
+    type            = each.value.type
+    zone_id         = aws_route53_zone.this.zone_id
+}
+
 resource "aws_acm_certificate_validation" "api_cert_val" {
     region = "us-east-2"
 
     certificate_arn         = aws_acm_certificate.api["us-east-2"].arn
+    validation_record_fqdns = [for record in aws_route53_record.api_cert : record.fqdn]
+}
+
+resource "aws_acm_certificate_validation" "api_cert_val_eu_west_1" {
+    region = "eu-west-1"
+
+    certificate_arn         = aws_acm_certificate.api["eu-west-1"].arn
     validation_record_fqdns = [for record in aws_route53_record.api_cert : record.fqdn]
 }
