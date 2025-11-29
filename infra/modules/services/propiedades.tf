064fb93f38dd2870a22e0ebe8d240a00d1f4f2f0
redsquirrelx <redsquirrel.xi@gmail.com>
2025-11-29 02:18:39 -0500

    feat(servicios): archivo por provisionamiento de servicio

---
+++ infra/modules/services/propiedades.tf
@@ -0,0 +1,62 @@
+module "propiedades" {
+    source = "../ecs-service"
+
+    name = "propiedades"
+
+    region = var.region
+    service_policy_document_json = data.aws_iam_policy_document.propiedades.json
+    container_definition_json = local.propiedades_container_definition
+    cluster_id = aws_ecs_cluster.this.id
+
+    security_group_id = var.service_data.propiedades.security_group_id
+    subnets_ids = var.service_data.propiedades.subnets_ids
+    target_group_arn = var.service_data.propiedades.target_group_arn
+}
+
+data "aws_iam_policy_document" "propiedades"{
+    statement {
+        sid = "AllowFargateDynamoDBAccess"
+        effect = "Allow"
+
+        # Acciones de un microservicio CRUD
+        actions = [
+            "dynamodb:GetItem",
+            "dynamodb:PutItem",
+            "dynamodb:UpdateItem",
+            "dynamodb:DeleteItem",
+            "dynamodb:Query", #Para consultar usando la llave primaria
+            "dynamodb:Scan" # Escanear toda la tabla (COstoso)
+        ]
+
+        # Recurso sobre el cual se aplica estas acciones
+        # Aca se coloca los ARNs
+        resources = [ 
+            "arn:aws:dynamodb:${var.region}:${var.account_id}:table/Propiedades"
+        ]
+    }
+}
+
+locals {
+    propiedades_container_definition = jsonencode([
+                {
+            name      = "propiedades"
+            image     = var.service_data.propiedades.repository_url
+            cpu       = 1
+            memory    = 1024
+            essential = true
+            portMappings = [
+                {
+                    containerPort = 80
+                    hostPort      = 80
+                }
+            ]
+            readonlyRootFilesystem = true
+            environment = [
+                {
+                    name = "SERVICE_REGION",
+                    value = var.region
+                }
+            ]
+        }
+    ])
+}
---
+++ infra/modules/services/reservas.tf
@@ -0,0 +1,70 @@
+module "reservas" {
+    source = "../ecs-service"
+    
+    name = "reservas"
+
+    region = var.region
+    service_policy_document_json = data.aws_iam_policy_document.reservas.json
+    container_definition_json = local.reservas_container_definition
+    cluster_id = aws_ecs_cluster.this.id
+
+    security_group_id = var.service_data.reservas.security_group_id
+    subnets_ids = var.service_data.reservas.subnets_ids
+    target_group_arn = var.service_data.reservas.target_group_arn
+}
+
+data "aws_iam_policy_document" "reservas"{
+    statement {
+        sid = "AllowFargateSNSAccess"
+        effect = "Allow"
+        
+        actions = [
+            "sns:Publish"
+        ]
+
+        resources = [ var.service_data.reservas.topic_arn ]
+    }
+
+    statement {
+        sid = "AllowFargateKMSAccess"
+        effect = "Allow"
+        
+        actions = [
+            "kms:GenerateDataKey",
+            "kms:Decrypt"
+        ]
+
+        resources = [
+            var.service_data.reservas.kms_arn
+        ]
+    }
+}
+
+locals {
+    reservas_container_definition = jsonencode([
+        {
+            name      = "reservas"
+            image     = var.service_data.reservas.repository_url
+            cpu       = 1
+            memory    = 1024
+            essential = true
+            portMappings = [
+                {
+                    containerPort = 80
+                    hostPort      = 80
+                }
+            ]
+            readonlyRootFilesystem = true
+            environment = [
+                {
+                    name = "RESERVAS_PROC_SNS_TOPIC_ARN",
+                    value = var.service_data.reservas.topic_arn
+                },
+                {
+                    name = "SERVICE_REGION",
+                    value = var.region
+                }
+            ]
+        }
+    ])
+}
