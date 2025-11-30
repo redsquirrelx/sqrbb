82cc6028a2922cc44bd73e67f5d3489ba1d91673
redsquirrelx <redsquirrel.xi@gmail.com>
2025-11-30 18:36:54 -0500

    feat(endpoints): logs endpoint

--- infra/modules/network/endpoints.tf
+++ infra/modules/network/endpoints.tf
@@ -63,6 +63,24 @@ resource "aws_vpc_endpoint" "email" {
     private_dns_enabled = true
 }
 
+resource "aws_vpc_endpoint" "log" {
+    vpc_id            = aws_vpc.this.id
+    service_name      = "com.amazonaws.${data.aws_region.current.region}.logs"
+    vpc_endpoint_type = "Interface"
+    subnet_ids        = [ aws_subnet.service-subnets[0].id ]
+    security_group_ids = [ aws_security_group.endpoints.id ]
+    private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "streamlogs" {
+    vpc_id            = aws_vpc.this.id
+    service_name      = "com.amazonaws.${data.aws_region.current.region}.stream-logs"
+    vpc_endpoint_type = "Interface"
+    subnet_ids        = [ aws_subnet.service-subnets[0].id ]
+    security_group_ids = [ aws_security_group.endpoints.id ]
+    private_dns_enabled = true
+}
+
 resource "aws_vpc_endpoint" "s3" {
     vpc_id            = aws_vpc.this.id
     service_name      = "com.amazonaws.${data.aws_region.current.region}.s3"
