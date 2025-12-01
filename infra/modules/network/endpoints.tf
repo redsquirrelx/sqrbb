d30bb2b0eb0abeac9dbf0ece6612fdc9a1ee1120
redsquirrelx <redsquirrel.xi@gmail.com>
2025-11-30 20:17:06 -0500

    revert(endpoints): eliminar stream-logs

--- infra/modules/network/endpoints.tf
+++ infra/modules/network/endpoints.tf
@@ -72,15 +72,6 @@ resource "aws_vpc_endpoint" "log" {
     private_dns_enabled = true
 }
 
-resource "aws_vpc_endpoint" "streamlogs" {
-    vpc_id            = aws_vpc.this.id
-    service_name      = "com.amazonaws.${data.aws_region.current.region}.stream-logs"
-    vpc_endpoint_type = "Interface"
-    subnet_ids        = [ aws_subnet.service-subnets[0].id ]
-    security_group_ids = [ aws_security_group.endpoints.id ]
-    private_dns_enabled = true
-}
-
 resource "aws_vpc_endpoint" "s3" {
     vpc_id            = aws_vpc.this.id
     service_name      = "com.amazonaws.${data.aws_region.current.region}.s3"
